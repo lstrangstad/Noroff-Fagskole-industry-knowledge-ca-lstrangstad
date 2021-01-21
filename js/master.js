e0c630f6c76ead4035819f07ff7d4cf0ea5b1dc4
Linus <linusstrangstad@gmail.com>
2021-01-21 16:12:26 +0100

    Feature: Typing function

---
+++ js/master.js
@@ -0,0 +1,20 @@
+const words = ["HTML", "CSS", "JavaScript", "React", "SASS"];
+let count = 0;
+let index = 0;
+let currentWord = "";
+let letter = "";
+
+(function typing() {
+  if (count === words.length) {
+    count = 0;
+  }
+  currentWord = words[count];
+  letter = currentWord.slice(0, ++index);
+
+  document.querySelector(".header__typing").textContent = letter;
+  if (letter.length == currentWord.length) {
+    count++;
+    index = 0;
+  }
+  setTimeout(typing, 350);
+})();
