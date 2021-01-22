f615b8be6b623f3eddd7090f293084623849c338
Linus <linusstrangstad@gmail.com>
2021-01-22 21:44:42 +0100

    Feature: Formvalidator and name validator function

--- js/master.js
+++ js/master.js
@@ -18,3 +18,31 @@ let letter = "";
   }
   setTimeout(typing, 350);
 })();
+
+const formOutput = document.querySelector(".contact__form");
+const errorMessage = document.querySelector(".error-message");
+
+formOutput.addEventListener("submit", formValidator);
+
+function formValidator(e) {
+  e.preventDefault();
+
+  const nameValue = document.querySelector("#nameInput").value;
+
+  if (nameValidator(nameValue) == !true) {
+    errorMessage.innerHTML = "Please enter name with at least two characters";
+  } else {
+    errorMessage.style.display = "none";
+  }
+}
+
+function nameValidator(string) {
+  const inputValue = string.trim();
+  if (inputValue.length > 2) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function emailValidator()
